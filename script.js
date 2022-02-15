55dc2836262b4a7716d259ddde74b7e2fbd16551
Shreeya <shreeya@Shreeyas-MacBook-Pro.local>
2022-02-15 16:14:30 +0000

    recomitting w/ add -redid grid and added js classes

---
+++ script.js
@@ -0,0 +1,32 @@
+//reqs: Accept a minimum of 2 inputs, perform an operation and return the output.
+
+//INPUT numbers
+  //function
+  //OUTPUT numbers in display on click
+
+//INPUT symbols/operators
+  //function
+  //OUTPUT first number + operator?
+
+//INPUT =
+  //OUTPUT total
+
+//INPUT +
+  //function is sum of input no 1 and input no 2 
+    //callback function: on click of = display total
+    //OUTPUT total  
+
+//INPUT -
+  //function is input no 1 - input no 2 
+    //callback function: on click of = display total
+    //OUTPUT total  
+
+//INPUT x
+  //function is input no 1 * input no 2 
+    //callback function: on click of = display total
+    //OUTPUT total   
+    
+//INPUT &divide
+  //function is (input no 1 *100) / input no 2 
+    //callback function: on click of = display total
+    //OUTPUT total     
