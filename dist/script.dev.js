f221290828b8967376f66f9e0fad5e1c48de25df
Shreeya <shreeya@Shreeyas-MacBook-Pro.local>
2022-02-16 14:50:22 +0000

    fixed classes and global variables

--- dist/script.dev.js
+++ dist/script.dev.js
@@ -1,7 +1,7 @@
+"use strict";
+
 //reqs: Accept a minimum of 2 inputs, perform an operation and return the output.
-//INPUT numbers
-//function
-//OUTPUT numbers in display on click
+// console.log number inputs by user
 //INPUT symbols/operators
 //function
 //OUTPUT first number + operator?
@@ -23,4 +23,23 @@
 //function is (input no 1 *100) / input no 2 
 //callback function: on click of = display total
 //OUTPUT total     
-"use strict";
+//
+// global variables     
+var numberButtons = document.querySelectorAll(".calc__button");
+var operationButtons = document.querySelectorAll(".calc__button--operator");
+var clearButton = document.querySelector(".calc__button--clear");
+var equalsButton = document.querySelector(".calc__button--equals");
+var previousDisplay = document.querySelector(".previous-display");
+var currentDisplay = document.querySelector(".current-display"); //functions
+//INPUT numbers
+//function
+//OUTPUT numbers in display on click
+
+var onButtonClick = function onButtonClick(event) {
+  console.log(event.target.value);
+  currentDisplay.innerHTML += event.target.value;
+};
+
+numberButtons.forEach(function (button) {
+  button.addEventListener("click", onButtonClick);
+});
--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 //reqs: Accept a minimum of 2 inputs, perform an operation and return the output.
 
-//INPUT numbers
-  //function
-  //OUTPUT numbers in display on click
+// console.log number inputs by user
+
+
 
 //INPUT symbols/operators
   //function
@@ -30,3 +30,29 @@
   //function is (input no 1 *100) / input no 2 
     //callback function: on click of = display total
     //OUTPUT total     
+//
+
+// global variables     
+const numberButtons = document.querySelectorAll (".calc__button");
+const operationButtons = document.querySelectorAll (".calc__button--operator");
+const clearButton = document.querySelector (".calc__button--clear")
+const equalsButton = document.querySelector (".calc__button--equals");
+const previousDisplay = document.querySelector (".previous-display");
+const currentDisplay = document.querySelector (".current-display");
+
+//functions
+
+
+//INPUT numbers
+  //function
+  //OUTPUT numbers in display on click
+
+const onButtonClick = (event) => {
+  console.log (event.target.value);
+  currentDisplay.innerHTML += (event.target.value);
+}
+  
+numberButtons.forEach ((button) => {
+  button.addEventListener("click",onButtonClick)
+})
+
